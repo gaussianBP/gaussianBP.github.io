205092a0358a7422af6030d4398f5e920c5cb5e6
Xiaofan Mu <xiaofan.mu19@imperial.ac.uk>
2020-06-17 09:48:23 +0800

    Now automatically update ids for all other nodes as a node is deleted, check for nodes with no connections, and some optimization

--- src/diagrams/playground.svelte
+++ src/diagrams/playground.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
   import * as pg from "../playground/playground.js";
   import { onInterval } from "../util.js";
   import { event as currentEvent } from "d3";
@@ -19,17 +20,15 @@
   var node_mousedown = null;
   var connection_mousedown = null;
   var node_clicked = null;
-  let node_double_click = false;
+  let last_node_clicked = false;
   var connection_clicked = null;
   var last_connection_clicked = null;
-  let connection_node1 = null;
-  let connection_node2 = null;
   let current_mouse_location = { x: null, y: null };
 
   // UI
-  let display_node_text = false;
-  let x_offset = 72;
-  let y_offset = 250;
+  let display_node_text = true;
+  let message = { message: null, timestamp: null, duration: null };
+  let check_connection_message = { message: null, timestamp: null, duration: null };
 
   $: var_nodes = [];
   $: factor_nodes = [];
@@ -37,16 +36,17 @@
 
   onMount(() => {
     graph = pg.createNewPlayground(n_var_nodes, n_factor_nodes);
-    // random_location();
-    update_playground();
+    reset_playground();
   });
 
   onInterval(() => update_playground(), 25);
 
   function update_playground() {
+    graph.update_node_id();
     var_nodes = graph.var_nodes;
     factor_nodes = graph.factor_nodes;
     update_connection();
+    update_messages();
   }
 
   function update_connection() {
@@ -70,20 +70,11 @@
   }
 
   function add_var_node() {
-    let location = random_location();
-    graph.add_var_node(location.x, location.y);
+    graph.add_var_node(100, 100);
   }
 
   function add_factor_node() {
-    let location = random_location();
-    graph.add_factor_node(location.x, location.y);
-  }
-
-  function random_location() {
-    return {
-      x: Math.random() * (svg_width - x_offset * 2) + x_offset,
-      y: Math.random() * (svg_height - y_offset * 2) + y_offset
-    };
+    graph.add_factor_node(100, 100);
   }
 
   function reset_playground() {
@@ -93,6 +84,18 @@
     connections = [];
     graph = pg.createNewPlayground(n_var_nodes, n_factor_nodes);
     update_playground();
+    update_web_element();
+  }
+
+  function find_connection(id) {
+    let connection = connections.filter(
+      connection => connection.id == parseInt(connection_clicked.id)
+    )[0];
+    if (connection) {
+      return connection;
+    } else {
+      return null;
+    }
   }
 
   function mousedown_handler(e) {
@@ -105,11 +108,12 @@
   }
 
   function mousemove_handler(e) {
+    const rect = e.currentTarget.getBoundingClientRect();
     current_mouse_location = {
-      x: e.clientX - x_offset,
-      y: e.clientY - y_offset
+      x: e.clientX - rect.x,
+      y: e.clientY - rect.y
     };
-    if (node_mousedown && !node_double_click) {
+    if (node_mousedown && !last_node_clicked) {
       node_mousedown.x = current_mouse_location.x;
       node_mousedown.y = current_mouse_location.y;
     }
@@ -121,58 +125,112 @@
   }
 
   function click_handler(e) {
+    // console.log(e.clientX, e.clientY);
     node_clicked = null;
     connection_clicked = null;
     node_clicked = e.path.find(element => element.classList == "g_node");
     connection_clicked = e.path.find(
-      element => element.classList == "g_connection"
+      element => element.classList == "line_connection"
     );
+    
     if (Date.now() - mousedown_time <= 100) {
       // If time between mousedown and mouseup is short, consider as a click
       if (node_clicked) {
         // When a node is clicked
-        if (!node_double_click) {
-          // Clicked first node, initiate adding connection, save current node as starting point
-          connection_node1 = graph.find_node(node_clicked.id);
-          node_double_click = true;
-        } else if (node_double_click) {
-          connection_node2 = graph.find_node(node_clicked.id);
-          if (connection_node1 == connection_node2) {
-            // Remove the node if double-clicked
+        node_clicked = graph.find_node(node_clicked.id);
+        if (!last_node_clicked) {
+          // No node clicked before, initiate adding connection, save current node as starting point
+          last_node_clicked = node_clicked;
+        } else {
+          // This is the second click
+          if (node_clicked == last_node_clicked) {
+            // Remove the node if double clicked the same node
             graph.remove_node(node_clicked.id);
-            node_double_click = false;
-            connection_node1 = null;
-            connection_node2 = null;
+            last_node_clicked = null;
+            node_clicked = null;
           } else if (
-            graph.add_node_connection(connection_node1.id, connection_node2.id)
+            graph.add_connection(last_node_clicked.id, node_clicked.id)
           ) {
             // Successfully added a new connection
-            node_double_click = false;
-            connection_node1 = null;
-            connection_node2 = null;
-          }
-        }
-      } else if (connection_clicked) {
-        if (last_connection_clicked == connection_clicked) {
-          let connection = connections.filter(
-            connection => connection.id == parseInt(connection_clicked.id)
-          )[0];
-          if (connection) {
-            graph.remove_node_connection(
-              connection.var_id,
-              connection.factor_id
+            console.log(
+              "Successfully added a new connection between",
+              last_node_clicked.id,
+              "and",
+              node_clicked.id
             );
+          } else {
+            // Unable to add new connection
+            console.log(
+              "Failed to add the connection between",
+              last_node_clicked.id,
+              "and",
+              node_clicked.id
+            );
+          }
+          last_node_clicked = null;
+          node_clicked = null;
+        }
+      } else if (connection_clicked && !node_clicked) {
+        // Clicked on a connection
+        if (last_connection_clicked == connection_clicked) {
+          // Doubleclick the same connection to remove it
+          connection_clicked = find_connection(connection_clicked.id);
+          if (connection_clicked) {
+            // If the connection is valid
+            graph.remove_connection(
+              connection_clicked.var_id,
+              connection_clicked.factor_id
+            );
+            console.log(e);
             update_playground();
           }
+          connection_clicked = null;
           last_connection_clicked = null;
         } else {
+          // First time clicking on a connection
           last_connection_clicked = connection_clicked;
         }
-      } else if (node_double_click && !node_clicked) {
+      } else if (last_node_clicked && !node_clicked) {
         // Did not click on a node while adding connection, cancel
-        node_double_click = false;
-        connection_node1 = null;
-        connection_node2 = null;
+        last_node_clicked = null;
+        node_clicked = null;
+      }
+    }
+  }
+
+  function check_connection() {
+    if (graph.check_connection()) {
+      check_connection_message = {
+        message: "Connections are good!",
+        timestamp: Date.now(),
+        duration: 3000
+      };
+    } else {
+      check_connection_message = {
+        message: "Missing connections!",
+        timestamp: Date.now(),
+        duration: 3000
+      };
+    }
+  }
+
+  function update_web_element() {
+    document.getElementById(
+      "toggle_node_text_display_checkbox"
+    ).checked = display_node_text;
+  }
+
+  function update_messages() {
+    if (message.message) {
+      if (Date.now() - message.timestamp >= message.duration) {
+        // Clear message if message has passed duration
+        message = { message: null, timestamp: null, duration: null };
+      }
+    }
+    if (check_connection_message.message) {
+      if (Date.now() - check_connection_message.timestamp >= check_connection_message.duration) {
+        // Clear message if message has passed duration
+        check_connection_message = { message: null, timestamp: null, duration: null };
       }
     }
   }
@@ -180,8 +238,6 @@
   function toggle_node_text_display() {
     display_node_text = !display_node_text;
   }
-
-  // on:dblclick={doubleclick_handler}
 </script>
 
 <style>
@@ -201,89 +257,101 @@
       bind:this={svg}
       width={svg_width}
       height={svg_height}
-      cursor="pointer"
       on:mousedown={mousedown_handler}
       on:mousemove={mousemove_handler}
       on:mouseup={mouseup_handler}
       on:click={click_handler}>
       {#each connections as connection}
         <g
-          class="g_connection"
-          id={connection.id}
-          transform="translate({connection.x1}
-          {connection.y1})"
+          cursor="pointer"
           draggable="true">
           <line
-            x1={0}
-            y1={0}
-            x2={connection.x2 - connection.x1}
-            y2={connection.y2 - connection.y1}
+            class="line_connection"
+            id={connection.id}
+            x1={connection.x1}
+            y1={connection.y1}
+            x2={connection.x2}
+            y2={connection.y2}
             stroke="#D3D3D3"
-            stroke-width="5" />
-          <line
-            x1={0}
-            y1={0}
-            x2={connection.x2 - connection.x1}
-            y2={connection.y2 - connection.y1}
-            stroke="black"
-            stroke-width="2" />
+            stroke-width="7" />
         </g>
       {/each}
-      {#if node_double_click}
+      {#each connections as connection}
+        <g cursor="pointer">
+          <line
+            x1={connection.x1}
+            y1={connection.y1}
+            x2={connection.x2}
+            y2={connection.y2}
+            stroke="black"
+            stroke-width="1" />
+        </g>
+      {/each}
+      {#if last_node_clicked}
         <line
-          x1={connection_node1.x}
-          y1={connection_node1.y}
+          x1={last_node_clicked.x}
+          y1={last_node_clicked.y}
           x2={current_mouse_location.x}
           y2={current_mouse_location.y}
           stroke="black"
-          stroke-width="2" />
+          stroke-width="1" />
       {/if}
-      {#each factor_nodes as factor_node}
-        <g
-          class="g_node"
-          id={factor_node.id}
-          transform="translate({factor_node.x}
-          {factor_node.y})"
-          draggable="true">
-          <rect
-            x={-10}
-            y={-10}
-            width={20}
-            height={20}
-            stroke={'blue'}
-            fill={'white'} />
-          {#if display_node_text}
-            <text
-              x={0}
-              y={5}
-              text-anchor={'middle'}
-              stroke="black"
-              font-size={14}>
-              {factor_node.id}
-            </text>
-          {/if}
-        </g>
-      {/each}
-      {#each var_nodes as var_node, i}
-        <g
-          class="g_node"
-          id={var_node.id}
-          transform="translate({var_node.x}
-          {var_node.y})"
-          draggable="true">
-          <circle cx={0} cy={0} r={10} stroke={'red'} fill={'white'} />
-          {#if display_node_text}
-            <text
-              x={0}
-              y={5}
-              text-anchor={'middle'}
-              stroke="black"
-              font-size={14}>
-              {var_node.id}
-            </text>
-          {/if}
-        </g>
-      {/each}
+      <g>
+        {#each factor_nodes as factor_node}
+          <g
+            class="g_node"
+            id={factor_node.id}
+            transform="translate({factor_node.x}
+            {factor_node.y})"
+            cursor="pointer"
+            draggable="true">
+            <rect
+              x={-10}
+              y={-10}
+              width={20}
+              height={20}
+              stroke={'blue'}
+              fill={'white'} />
+            {#if display_node_text}
+              <g cursor="pointer">
+                <text
+                  x={0}
+                  y={5}
+                  text-anchor={'middle'}
+                  stroke="black"
+                  font-size={14}
+                  style="user-select: none">
+                  {factor_node.id}
+                </text>
+              </g>
+            {/if}
+          </g>
+        {/each}
+        {#each var_nodes as var_node, i}
+          <g
+            class="g_node"
+            id={var_node.id}
+            transform="translate({var_node.x}
+            {var_node.y})"
+            cursor="pointer"
+            draggable="true">
+            <circle cx={0} cy={0} r={10} stroke={'red'} fill={'white'} />
+            {#if display_node_text}
+              <g class="unselectable">
+                <text
+                  x={0}
+                  y={5}
+                  text-anchor={'middle'}
+                  stroke="black"
+                  font-size={14}
+                  style="user-select: none">
+                  {var_node.id}
+                </text>
+              </g>
+            {/if}
+          </g>
+        {/each}
+      </g>
     </svg>
   </div>
 
@@ -291,7 +359,7 @@
     <label>
       <button
         type="button"
-        class="btn btn-primary"
+        class="btn"
         on:click={add_var_node}
         style="width:200px; border:2px solid black">
         Add Variable Node
@@ -300,7 +368,7 @@
     <label>
       <button
         type="button"
-        class="btn btn-primary"
+        class="btn"
         on:click={add_factor_node}
         style="width:200px; border:2px solid black">
         Add Factor Node
@@ -309,18 +377,37 @@
     <label>
       <button
         type="button"
-        class="btn btn-primary"
+        class="btn"
+        on:click={check_connection}
+        style="width:200px; border:2px solid black">
+        {#if check_connection_message.message}
+          <p font-size=12 style="display:inline; color:red">{check_connection_message.message}</p>
+        {:else}
+          <p font-size=12 style="display:inline; color:black">Check Connection</p>
+        {/if}
+      </button>
+    </label>
+    <label>
+      <button
+        type="button"
+        class="btn"
         on:click={reset_playground}
         style="width:200px; border:2px solid black">
         Reset Playground
       </button>
     </label>
-    <label>
+    <label style="user-select: none">
       <input
         type="checkbox"
+        id="toggle_node_text_display_checkbox"
         value={display_node_text}
         on:click={toggle_node_text_display} />
       Toggle Node Text
     </label>
+    <div>
+        {#if message.message}
+          <p transition:fade font-size=14>{message.message}</p>
+        {/if}
+      </div>
   </div>
 </div>
--- src/playground/playground.js
+++ src/playground/playground.js
@@ -40,11 +40,11 @@ export class FactorGraph {
 
   find_node(id) {
     id = parseInt(id);
-    if (this.var_nodes.map(var_node => var_node.id).includes(parseInt(id))) {
-      return this.var_nodes.filter(var_node => var_node.id == parseInt(id))[0];
+    if (this.var_nodes.map(var_node => var_node.id).includes(id)) {
+      return this.var_nodes.filter(var_node => var_node.id == id)[0];
     }
-    else if (this.factor_nodes.map(factor_node => factor_node.id).includes(parseInt(id))) {
-      return this.factor_nodes.filter(factor_node => factor_node.id == parseInt(id))[0];
+    else if (this.factor_nodes.map(factor_node => factor_node.id).includes(id)) {
+      return this.factor_nodes.filter(factor_node => factor_node.id == id)[0];
     }
     else {
       return false;
@@ -53,20 +53,37 @@ export class FactorGraph {
 
   remove_node(id) {
     id = parseInt(id);
-    if (this.var_nodes.map(var_node => var_node.id).includes(parseInt(id))) {
-
-      this.var_nodes.pop(this.var_nodes.findIndex(var_node => var_node.id == parseInt(id)));
+    if (this.var_nodes.map(var_node => var_node.id).includes(id)) {
+      this.var_nodes.splice(this.var_nodes.findIndex(var_node => var_node.id == id), 1);
     }
-    else if (this.factor_nodes.map(factor_node => factor_node.id).includes(parseInt(id))) {
-      this.factor_nodes.pop(this.factor_nodes.findIndex(factor_node => factor_node.id == parseInt(id)));
-      for (let i = 0; i < this.var_nodes.length; i ++) {
-        this.var_nodes[i].adj_factor_ids.filter(factor_id => factor_id != parseInt(id));
+    else if (this.factor_nodes.map(factor_node => factor_node.id).includes(id)) {
+      this.factor_nodes.splice(this.factor_nodes.findIndex(factor_node => factor_node.id == id), 1);
+      for (let i = 0; i < this.var_nodes.length; i++) {
+        this.var_nodes[i].adj_factor_ids.filter(factor_id => factor_id != id);
       }
     }
   }
 
+  update_node_id() {
+    let ids = this.var_nodes.map(var_node => var_node.id)
+      .concat(this.factor_nodes.map(factor_node => factor_node.id));  // Collect all ids
+    ids = ids.sort((a, b) => a - b);  // Sort all ids by ascending order
+    let id_diff = new Array(ids[ids.length - 1]);  // Create array with length matching the largest id
+    let running_sum = 0;  // Use running sum to reduce loops
+    id_diff[0] = 0;
+    for (let i = 1; i < ids.length; i++) {
+      running_sum = running_sum + ids[i] - ids[i - 1] - 1;
+      id_diff[ids[i]] = running_sum;
+    }
+    for (let i = 0; i < this.var_nodes.length; i++) {
+      this.var_nodes[i].id = this.var_nodes[i].id - id_diff[this.var_nodes[i].id];
+    }
+    for (let i = 0; i < this.factor_nodes.length; i++) {
+      this.factor_nodes[i].id = this.factor_nodes[i].id - id_diff[this.factor_nodes[i].id];
+    }
+  }
+
   find_element(x, y) {
-    let element;
     for (let i = 0; i < this.var_nodes.length; i++) {
       if (Math.sqrt(Math.pow(this.var_nodes[i].x - x, 2) +
         Math.pow(this.var_nodes[i].y - y, 2)) <= interaction_distance_tolerance) {
@@ -84,7 +101,7 @@ export class FactorGraph {
     }
   }
 
-  add_node_connection(node1_id, node2_id) {
+  add_connection(node1_id, node2_id) {
     node1_id = parseInt(node1_id);
     node2_id = parseInt(node2_id);
     let node1 = this.find_node(node1_id);
@@ -106,20 +123,20 @@ export class FactorGraph {
     }
   }
 
-  remove_node_connection(node1_id, node2_id) {
+  remove_connection(node1_id, node2_id) {
     node1_id = parseInt(node1_id);
     node2_id = parseInt(node2_id);
     let node1 = this.find_node(node1_id);
     let node2 = this.find_node(node2_id);
     if (node1.type == 'var_node' && node2.type == 'factor_node') {
       if (node1.adj_factor_ids.includes(node2_id)) {  // If node2 is not already connected
-        node1.adj_factor_ids.pop(node1.adj_factor_ids.indexOf(node2_id));
+        node1.adj_factor_ids.splice(node1.adj_factor_ids.indexOf(node2_id), 1);
         return true;
       }
     }
     else if (node1.type == 'factor_node' && node2.type == 'var_node') {
       if (node2.adj_factor_ids.includes(node1_id)) {  // If node2 is not already connected
-        node2.adj_factor_ids.pop(node2.adj_factor_ids.indexOf(node1_id));
+        node2.adj_factor_ids.splice(node2.adj_factor_ids.indexOf(node1_id), 1);
         return true;
       }
     }
@@ -128,18 +145,18 @@ export class FactorGraph {
     }
   }
 
-  check_node_connection() {
+  check_connection() {
     // Check if all nodes are connected to neighbors
-    if (this.var_nodes.some(var_node =>  // Variable nodes with no adjacent factor node
-      var_node.adj_factor_ids.length == 0) || // or
+    let adj_factor_ids = this.var_nodes.map(var_node => var_node.adj_factor_ids).flat();
+    if (this.var_nodes.every(var_node =>  // Every variable node has adjacent factor nodes
+      var_node.adj_factor_ids.length != 0) &&
       this.factor_nodes.every(factor_node =>
-        this.var_nodes.map(var_node => var_node.adj_factor_ids)  // Factor nodes that are connected to variable nodes
-          .includes(factor_node.id))  // Check against all factor nodes
+        adj_factor_ids.includes(factor_node.id))  // Factor nodes that are connected to variable nodes
     ) {
-      return false;
+      return true;
     }
     else {
-      return true
+      return false;
     }
   }
 }
@@ -188,7 +205,7 @@ export function createNewPlayground(n_var_nodes = 2, n_factor_nodes = 1) {
   }
   for (let i = 0; i < n_var_nodes; i++) {
     for (let j = 0; j < n_factor_nodes; j++) {
-      graph.add_node_connection(graph.var_nodes[i].id, graph.factor_nodes[j].id);
+      graph.add_connection(graph.var_nodes[i].id, graph.factor_nodes[j].id);
     }
   }
   console.log(graph);
